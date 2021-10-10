62faca87b687bfe8c8c1f34c49e5216ffef7ddaf
isotop786 <marufislam77@gmail.com>
2021-10-10 12:07:49 +0600

    fix

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -145,3 +145,8 @@ STATICFILES_STORAGE = 'whitenoise.django.G\ipManif'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
